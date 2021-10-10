d6151c0c7bca0b2d54588a8c3e4ab1c500d42239
KT <prooffek@gmail.com>
2021-10-10 13:53:11 +0200

    automatic stages assignment depending on game name implemented

--- greco-field-game/src/Classes/Game.js
+++ greco-field-game/src/Classes/Game.js
@@ -1,12 +1,29 @@
+import {_gameNamesDict} from "../Utilities/_gameNamesDict";
+import {GameStage} from "./GameStage";
+import {_mapImports} from "../Utilities/_mapDict";
+import {_questions} from "../Utilities/_questionsDicts";
+
 export class Game {
     #gameName;
     #player;
     #stages;
     
-    constructor(name, player, stages) {
-        this.#gameName = name;
+    constructor(gameName, player) {
+        this.#gameName = gameName;
         this.#player = player;
-        this.#stages = stages
+        this.#stages = this.setStages(gameName);
+    }
+    
+    setStages(gameName) {
+        let i = 0;
+        const stages = [];
+
+        for (const [key, value] of _questions[gameName]){
+            stages.push(new GameStage(i, _mapImports[`map_${i}`], value))
+            i++;
+        }
+        
+        return stages;
     }
     
     getName() {
