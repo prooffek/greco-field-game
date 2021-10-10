201f55ddd9430adfc8747e2ba0df3a1b072b5505
KT <prooffek@gmail.com>
2021-10-10 17:40:14 +0200

    Welcome page component added

---
+++ greco-field-game/src/Components/WelcomePage.js
@@ -0,0 +1,21 @@
+﻿import header from "./../img/page-title-cropped.png";
+import map from "./../img/welcom-page-map.png";
+
+import "./WelcomePage.css";
+
+const WelcomePage = (props) => {
+    const changePage = () => {
+        console.log("clicked");
+        props.setNextPage(props.currentPage + 1);
+    }
+    
+    return(
+        <div className="welcome-page fixed-page-container">
+            <img className="welcome-page-header" src={header} alt="welcome-page-header" />
+            <img className="welcome-page-map" src={map} alt="welcome-page-map" />
+            <button type="button" onClick={changePage} className="start-btn">Start</button>
+        </div>
+    )
+}
+
+export default WelcomePage;
