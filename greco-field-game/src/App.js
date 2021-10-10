a69b5bca296f1e26d1875acc742c36b963823251
KT <prooffek@gmail.com>
2021-10-10 17:40:57 +0200

    App component adjusted to games needs (part I)

--- greco-field-game/src/App.js
+++ greco-field-game/src/App.js
@@ -1,24 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
+import {Game} from "./Classes/Game";
+import {useState} from "react";
+import GameComponent from "./Components/GameComponent";
+import WelcomePage from "./Components/WelcomePage";
+
+const gamePhases = {
+  welcomePage: 0,
+  game: 1,
+  summary: 2
+}
 
 function App() {
+  const [game, setGame] = useState(new Game());
+  const [phase, setPhase] = useState(gamePhases.welcomePage);
+  
+  // console.log("game: ", game);
+  // console.log("phase: ", phase);
+  
+  function phaseHandler(modifier) {
+      console.log("re-rendering");
+      setPhase(phase + modifier)
+  }
+
+  // console.log("render: ", phase === gamePhases.welcomePage);
+  // console.log("render: ", phase === gamePhases.game);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="page-container">
+        {
+          phase === gamePhases.welcomePage &&
+          <WelcomePage
+              setNextPage={setPhase}
+              currentPage={phase}
+          />
+        }
+        {
+          phase === gamePhases.game &&
+          <GameComponent
+              game={game}
+              currentPhase={phase}
+              setPhase={phaseHandler}
+              // addAnswer={setPlayer}
+              // answers={player.answers}
+          />
+        }
+      </div>
   );
 }
 
