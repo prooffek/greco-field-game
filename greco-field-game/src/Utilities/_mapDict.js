5f205ab9b941effcfdfc046094a5b5c71554a0b5
KT <prooffek@gmail.com>
2021-10-10 11:30:22 +0200

    mapDict utility added

---
+++ greco-field-game/src/Utilities/_mapDict.js
@@ -0,0 +1,19 @@
+import map_0 from "../../public/videos/step_0.mp4";
+import map_1 from "../../public/videos/step_1.mp4";
+import map_2 from "../../public/videos/step_2.mp4";
+import map_3 from "../../public/videos/step_3.mp4";
+import map_4 from "../../public/videos/step_4.mp4";
+import map_5 from "../../public/videos/step_5.mp4";
+import map_6 from "../../public/videos/step_6.mp4";
+import map_7 from "../../public/videos/step_7.mp4";
+
+export const mapImports = {
+    map_1: map_0,
+    map_2: map_1,
+    map_3: map_2,
+    map_4: map_3,
+    map_5: map_4,
+    map_6: map_5,
+    map_7: map_6,
+    map_8: map_7,
+}
