b8970e8259f4b197aca21dce0a62c0909515fc9a
KT <prooffek@gmail.com>
2021-10-10 11:30:36 +0200

    GameStage class added

---
+++ greco-field-game/src/Classes/GameStage.js
@@ -0,0 +1,32 @@
+export class GameStage {
+    #index;
+    #map;
+    #questions;
+    
+    constructor(index, map, questions) {
+        this.#index = index;
+        this.#map = map;
+        this.#questions = questions;
+    }
+    
+    getStageIndex(){
+        return this.#index;
+    }
+    
+    getMap() {
+        return this.#map;
+    }
+
+    getQuestionsAmount(){
+        return this.#questions.length;
+    }
+    
+    getQuestionObj(index){
+        if (!index < 0 || index >= this.getQuestionsAmount()){
+            return this.#questions[index];
+        }
+        return null;
+    }
+    
+    
+}
