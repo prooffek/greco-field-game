c85ea0d52051341eab663e3de245b3e1a2a1332d
KT <prooffek@gmail.com>
2021-10-12 15:17:09 +0200

    answers list implemented in the Question class

--- greco-field-game/src/Classes/Question.js
+++ greco-field-game/src/Classes/Question.js
@@ -1,12 +1,17 @@
+import {getRandomEl, shuffleList} from "../Utilities/_utilityFunctions";
+import {_answers} from "../Utilities/_wrongAnswersDict";
+
 export class Question {
     #id;
     #text;
     #correctAnswer;
+    #answersList;
 
     constructor(id, text, correctAnswer) {
         this.#id = id;
         this.#text = text;
         this.#correctAnswer = correctAnswer;
+        this.#answersList = this.setAnswersList();
     }
 
     getId() {
@@ -23,5 +28,15 @@ export class Question {
     
     isAnswerCorrect(answer){
         return answer === this.#correctAnswer;
+    }
+    
+    setAnswersList() {
+        let answers = [this.#correctAnswer, ...getRandomEl(3, _answers)]
+        shuffleList(answers);
+        return answers;
+    }
+    
+    getAnswersList() {
+        return this.#answersList;
     }
 }
