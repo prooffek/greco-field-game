c4482caaa7466c0b3033d15eaf4b1aa79f3a08da
KT <prooffek@gmail.com>
2021-10-12 15:17:52 +0200

    players answers management improved and fixed

--- greco-field-game/src/Classes/Player.js
+++ greco-field-game/src/Classes/Player.js
@@ -1,3 +1,5 @@
+import {removeDuplicatedAnswer} from "../Utilities/_utilityFunctions";
+
 export class Player {
     #name;
     #answers;
@@ -27,16 +29,17 @@ export class Player {
     }
     
     addAnswer(answer) {
-        let prevAnswer = this.#answers.filter(a => a.questionId === answer.questionId);
-        
-        if (prevAnswer.length > 0){
-            this.#answers.pop(prevAnswer);
-        }
-        
+        let prevAnswer = this.#answers.filter(a => a.questionId === answer.questionId)[0];
+        this.#answers = removeDuplicatedAnswer(this.#answers, prevAnswer);
         this.#answers.push(answer);
     }
     
     getAnswers() {
         return this.#answers;
+    }
+    
+    getAnswer(questionId) {
+        let answer = this.#answers.filter(a => a.questionId === questionId)[0];
+        return answer ? answer.answer : null;
     }
 }
