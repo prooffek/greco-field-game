db6647b9af124a2f4020be7ca3ec0f25c27a502b
KT <prooffek@gmail.com>
2021-10-15 00:07:19 +0200

    SummaryPage adjusted to display questions and answers

--- greco-field-game/src/Components/SummaryComponent/SummaryPage.js
+++ greco-field-game/src/Components/SummaryComponent/SummaryPage.js
@@ -1,14 +1,18 @@
 import "./SummaryPage.css";
 
 export default function SummaryPage(props) {
-    const player = props.player;
-    const setPhase = props.setPhase;
-    const phase = props.currentPhase
+    const game = props.game;
+    const player = game.getPlayer();
+    const questions = game.getAllQuestions();
+    console.log("answer", player.getAnswer(questions[0].getId()));
+    
+    // const setPhase = props.setPhase;
+    // const phase = props.currentPhase
     
     
-    function onClickHandler() {
-        setPhase(phase - 1);
-    }
+    // function onClickHandler() {
+    //     setPhase(phase - 1);
+    // }
     
     return(
         <section className="summary-page">
@@ -16,6 +20,16 @@ export default function SummaryPage(props) {
                 <div className="result-container">
                     <h1>Your result:</h1>
                     <p>{player.getScore()}</p>
+                </div>
+                <div className="result-container">
+                    {questions.map(question =>
+                        <div>
+                            <h2>Question {question.getId() + 1}</h2>
+                            <h3>{question.getQuestionText()}</h3>
+                            <p>Correct answer: {question.getCorrectAnswer()}</p>
+                            <p>Your answer: {player.getAnswer(question.getId())}</p>
+                        </div>
+                    )}
                 </div>
             </div>
         </section>
