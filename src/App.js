0a75cd430b52cf19b5d8f253bdda32206ecd2ed8
KT <prooffek@gmail.com>
2021-11-07 23:37:36 +0100

    English set as the default language

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ const gameName = _gameNamesDict.Italy.Gaddiciano;
 const newPlayer = new Player();
 
 const initState = {
-  [_state.game]: new Game(gameName, newPlayer, _languages.polish),
+  [_state.game]: new Game(gameName, newPlayer, _languages.english),
   [_state.phase]: gamePhases.welcomePage
 }
 
